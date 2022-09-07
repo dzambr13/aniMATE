f21d8ddaf64345e52791805020ccc3863afdf396
dzambr13 <dayvuhz@gmail.com>
2022-09-07 09:56:46 -0700

    db created

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,14 @@
 const Pics = require('../models/pics')
 const Account = require('../models/account')
 
-const createAccount = async (req, res) => {}
+const createAccount = async (req, res) => {
+  try {
+    const account = await new Account(req.body)
+    await account.save()
+    return res.status(201).json({
+      account
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
